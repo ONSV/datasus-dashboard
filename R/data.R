606475614210d36751d4295c3f29e21e8c2d778e
Jota Saraiva <jpsaraiva2002@gmail.com>
2024-01-19 10:55:03 -0300

    Add data prep and save scripts

--- R/data.R
+++ R/data.R
@@ -1,14 +1,22 @@
 library(tidyverse)
 library(geobr)
-library(sf)
 library(here)
 
 # salvar dados geospaciais
 
 estados <- read_state()
-municipios <- read_municipality()
+municipality <- read_municipality()
+municipios <- mutate(municipality, 
+                     code_muni = str_sub(as.character(code_muni),1,6))
 regioes <- read_region()
 
 save(estados, file = here("data", "estados.rda"))
 save(municipios,file = here("data", "municipios.rda"))
 save(regioes,file = here("data", "regioes.rda"))
+
+# salvando rtdeaths para melhorar performance
+
+library(roadtrafficdeaths)
+save(rtdeaths, file = here("data","rtdeaths.rda"))
+
+
