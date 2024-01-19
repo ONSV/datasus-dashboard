cd6ef425ad8934d41fd26b5a50f9aff6ef5420ad
Jota Saraiva <jpsaraiva2002@gmail.com>
2024-01-19 16:26:55 -0300

    Fix pyramid plot, adds time series plot

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(here)
 library(sf)
 library(leaflet)
+library(plotly)
+library(onsvplot)
 
 load(here("data","estados.rda"))
 load(here("data","municipios.rda"))
@@ -16,12 +18,67 @@ prep_pyramid <- function(data, year, cod) {
     rename(code_muni = cod_municipio_ocor) |> 
     relocate(code_muni) |>  
     filter(ano_ocorrencia == year) |> 
-    count(code_muni, faixa_etaria_vitima, sexo_vitima, name = "mortes") |> 
+    left_join(x = municipios, by = "code_muni") |> 
+    st_drop_geometry() |> 
     filter(code_muni == cod) |> 
-    complete(sexo_vitima, 
-             faixa_etaria_vitima = unique(data$faixa_etaria_vitima),
+    count(faixa_etaria_vitima, sexo_vitima, name = "mortes") |> 
+    complete(faixa_etaria_vitima = unique(data$faixa_etaria_vitima),
+             sexo_vitima = unique(data$sexo_vitima),
              fill = list(mortes = 0)) |> 
-    select(-code_muni)
-    
-  return(res)
+    drop_na()
+  
+  max_value = max(res$mortes)
+  
+  plot <- res |> 
+    mutate(mortes = ifelse(test = sexo_vitima == "Masculino", 
+                           yes = -mortes, no = mortes)) |> 
+    mutate(abs_mortes = abs(mortes)) |> 
+    plot_ly(x = ~mortes, y = ~faixa_etaria_vitima, 
+            color = ~sexo_vitima, colors = c(onsv_palette$blue, onsv_palette$yellow)) |> 
+    add_bars(orientation = 'h', hoverinfo = "text", text = ~abs_mortes,
+             textposition = "none") |> 
+    layout(bargap = 0.1, barmode = 'overlay',
+           xaxis = list(tickmode = 'array', 
+                        tickvals = c(-(max_value),-max_value/2,0,max_value/2,max_value),
+                        ticktext = c(toString(max_value),
+                                     toString(max_value/2),
+                                     toString(0),
+                                     toString(max_value/2),
+                                     toString(max_value)),
+                        range = c(-max_value, max_value),
+                        title = ""),
+           yaxis = list(title = "", ticklen = 3, tickcolor = "white"))
+  return(plot)
 }
+
+# script para serie temporal
+
+prep_ts <- function(data, year, cod) {
+  res <-
+    tibble(data) |> 
+    rename(code_muni = cod_municipio_ocor) |> 
+    filter(ano_ocorrencia == year) |> 
+    left_join(x = municipios, by = "code_muni") |> 
+    st_drop_geometry() |> 
+    filter(code_muni == cod) |> 
+    count(data_ocorrencia, name = "mortes") |> 
+    arrange(data_ocorrencia) |> 
+    complete(data_ocorrencia = unique(data$data_ocorrencia), 
+             fill = list(mortes = 0)) |> 
+    filter(year(data_ocorrencia) == year)
+  
+  plot <- res |> 
+    plot_ly(type = 'scatter', mode = "lines") |> 
+    add_trace(x = ~data_ocorrencia, y = ~mortes, fill = "tozeroy",
+              line = list(color = onsv_palette$blue, width = 1),
+              fillcolor = 'rgba(0, 73, 110, 0.80)', hoverinfo = 'text',
+              text = ~paste(mortes,"vítimas")) |> 
+    layout(showlegend = F,
+           xaxis = list(title = ""),
+           yaxis = list(title = ""))
+  
+  return(plot)
+}
+
+prep_ts(rtdeaths, 2020, "353060")
+
